9af8a7565e0638206b001057beaef8bdf2a53b18
goddardduncan <64201397+goddardduncan@users.noreply.github.com>
2024-03-03 15:47:43 +1100

    Update player.js

--- player.js
+++ player.js
@@ -62,7 +62,8 @@ function playM3u8(url) {
     video.play();
   });
   hls.on(Hls.Events.FRAG_PARSING_METADATA, handleTimedMetadata);
-  document.title = 'ABC';
+  var chan = 'ABC';
+  document.title = chan;
 }
 
 debug = false;
