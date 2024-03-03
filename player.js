5e213b3f04f90bf81a538dca11bb6cb7f0847414
goddardduncan <64201397+goddardduncan@users.noreply.github.com>
2024-03-03 12:54:58 +1100

    Update player.js

--- player.js
+++ player.js
@@ -51,7 +51,7 @@ function playM3u8(url) {
   hls = new Hls({ debug: debug });
 
   // Use CORS Proxy
-  var corsProxy = 'https://api.allorigins.win/raw?url='; // Specify the CORS proxy URL
+  var corsProxy = 'https://thingproxy.freeboard.io/fetch/'; // Specify the CORS proxy URL
   var m3u8Url = decodeURIComponent(url);
   var proxiedUrl = corsProxy + m3u8Url; // Prepend the proxy URL to the actual URL
 
